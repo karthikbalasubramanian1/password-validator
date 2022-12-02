e273ea8e3c6b4bf1f52e1b161379a8720e35eca9
Karthik <mailkarthikb90@gmail.com>
2022-12-02 06:28:22 +0000

    Revert "Create validation result"
    This reverts commit fba095e5083abd7100dec4c36ff6d6d2d82b321c.

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
 # password-validator
-
--- src/main/java/com/example/domain/PasswordCriteria.java
+++
@@ -1,9 +0,0 @@
-package com.example.domain;
-
-/**
- * A generic abstraction for implementing various password validation criteria
- */
-public interface PasswordCriteria {
-
-    ValidationResult validate(Password password);
-}
