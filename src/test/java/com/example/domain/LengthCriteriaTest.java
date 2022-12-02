fba095e5083abd7100dec4c36ff6d6d2d82b321c
Karthik <mailkarthikb90@gmail.com>
2022-12-02 06:24:48 +0000

    Create validation result Add length criteria for the password check

---
+++ src/test/java/com/example/domain/LengthCriteriaTest.java
@@ -0,0 +1,26 @@
+package com.example.domain;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class LengthCriteriaTest {
+
+    @Test
+    @DisplayName("Validate password creation with minimum length")
+    void testPasswordWithValidLength() {
+        Password longPassword = Password.createPassword("Password 1234");
+        ValidationResult result = new LengthCriteria().validate(longPassword);
+        Assertions.assertTrue(result.isValid());
+    }
+
+    @Test
+    @DisplayName("Validate error with short text")
+    void testShortPassword() {
+        Password shortPassword = Password.createPassword("Pass");
+        ValidationResult result = new LengthCriteria().validate(shortPassword);
+        Assertions.assertFalse(result.isValid());
+        Assertions.assertEquals("Password length should be at least 8", result.getResult());
+    }
+
+}
